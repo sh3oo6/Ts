ea874717d683b2b10df548751463eb6bec7e5c90
sh3oo6 <141688473+sh3oo6@users.noreply.github.com>
2023-08-28 00:30:09 +0300

    Update dado.py

--- dado.py
+++ dado.py
@@ -11,11 +11,11 @@ for t in range(3333):
         i += +1
         x += +1
         sleep(0.1)
-        if i == 500:
-            sleep(1)
+        if i == 270:
+            sleep(2)
             break
-        req = requests.get(f"https://t.me/{user}").text
-        if ('If you have <strong>Telegram</strong>, you can contact <a class="tgme_username_link"') in req:
+        req = requests.get(f"https://t.me/{user}")
+        if req.text.find('If you have <strong>Telegram</strong>, you can contact <a class="tgme_username_link"') >= 0:
             print('yes')
             sleep(50000)
         else:
