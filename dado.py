f37732844957ab21a555410583e1af0aa221339f
sh3oo6 <141688473+sh3oo6@users.noreply.github.com>
2023-09-09 05:29:45 +0300

    Update dado.py

--- dado.py
+++ dado.py
@@ -1,5 +1,5 @@
-import httpx
 from time import sleep; import requests
+import time
 from telethon.tl.functions.account import UpdateUsernameRequest
 from telethon import TelegramClient , functions
 import asyncio
@@ -7,14 +7,17 @@ user = 'iiiidida'
 x = 0
 for t in range(3333):
     i = 0
+    start_time = time.time()
     for t in range(3333):
         i += +1
         x += +1
-        sleep(0.0001)
-        if i == 270:
-            sleep(2)
+        if i == 299:
+            end_time = time.time()  # وقت انتهاء تنفيذ الكود
+            execution_time = end_time - start_time  # الوقت المستغرق في تنفيذ الكود بالثواني
+            print(f"الوقت المستغرق: {int(execution_time)} ثواني")
+            sleep(61-int(execution_time))
             break
-        req = httpx.get(f"https://t.me/{user}")
+        req = requests.get(f"https://t.me/{user}")
         if ('If you have <strong>Telegram</strong>, you can contact <a class="tgme_username_link"') in req.text :
             print('yes')
             sleep(50000)
